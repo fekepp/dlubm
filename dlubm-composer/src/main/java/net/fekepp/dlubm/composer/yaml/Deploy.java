81ac8115212378792c0c40bb9ee36f8e76427a44
Felix Leif Keppmann <felix.leif@keppmann.de>
2017-05-14 22:28:33 +0200

    Added department amounts and placement constraints

--- dlubm-composer/src/main/java/net/fekepp/dlubm/composer/yaml/Deploy.java
+++ dlubm-composer/src/main/java/net/fekepp/dlubm/composer/yaml/Deploy.java
@@ -4,12 +4,22 @@ public class Deploy {
 
 	private String[] labels;
 
+	private Placement placement;
+
 	public String[] getLabels() {
 		return labels;
 	}
 
 	public void setLabels(String[] labels) {
 		this.labels = labels;
+	}
+
+	public Placement getPlacement() {
+		return placement;
+	}
+
+	public void setPlacement(Placement placement) {
+		this.placement = placement;
 	}
 
 }
---
+++ dlubm-composer/src/main/java/net/fekepp/dlubm/composer/yaml/Placement.java
@@ -0,0 +1,15 @@
+package net.fekepp.dlubm.composer.yaml;
+
+public class Placement {
+
+	private String[] constraints;
+
+	public String[] getConstraints() {
+		return constraints;
+	}
+
+	public void setConstraints(String[] constraints) {
+		this.constraints = constraints;
+	}
+
+}
