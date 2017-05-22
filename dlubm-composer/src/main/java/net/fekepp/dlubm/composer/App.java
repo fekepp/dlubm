2d23016cad66da46e6f15f0c7f3c997341ad0496
Felix Leif Keppmann <felix.leif@keppmann.de>
2017-05-22 14:25:39 +0200

    Added handling of parameters to cmd cli

--- dlubm-composer/src/main/java/net/fekepp/dlubm/composer/App.java
+++ dlubm-composer/src/main/java/net/fekepp/dlubm/composer/App.java
@@ -11,23 +11,74 @@ import org.slf4j.bridge.SLF4JBridgeHandler;
 
 public class App {
 
-	public static final String APP_NAME = "scal-server";
+	public static final String APP_NAME = "dlubm-composer";
 
-	public static final Option OPTION_PORT = Option.builder("p").longOpt("port").desc("port to be used by the server")
-			.hasArg().type(Number.class).required().build();
+	public static final Option OPTION_IMAGE_ONTOLOGY = Option.builder("io").longOpt("image-ontology")
+			.desc("Image for ongtology").hasArg().type(String.class).required(false).build();
 
-	public static final Option OPTION_STORAGE_DIRECTORY = Option.builder("s").longOpt("storage-directory")
-			.desc("storage directory to be used by the server").hasArg().type(String.class).required().build();
+	public static final Option OPTION_IMAGE_GLOBAL = Option.builder("ig").longOpt("image-global")
+			.desc("Image for global level").hasArg().type(String.class).required(false).build();
 
-	public static final Option OPTION_TEMPORARY_STORAGE_DIRECTORY = Option.builder("t")
-			.longOpt("temporary-storage-directory").desc("temporary storage directory to be used by the server")
+	public static final Option OPTION_IMAGE_UNIVERSITY = Option.builder("iu").longOpt("image-university")
+			.desc("Image for university level").hasArg().type(String.class).required(false).build();
+
+	public static final Option OPTION_IMAGE_DEPARTMENT = Option.builder("id").longOpt("image-department")
+			.desc("Image for department level").hasArg().type(String.class).required(false).build();
+
+	public static final Option OPTION_NETWORK = Option.builder("n").longOpt("network").desc("Reverse proxy network")
 			.hasArg().type(String.class).required().build();
+
+	public static final Option OPTION_DOMAIN = Option.builder("d").longOpt("domain").desc("Reverse proxy domain")
+			.hasArg().type(String.class).required().build();
+
+	public static final Option OPTION_DOMAIN_GLOBAL = Option.builder("dg").longOpt("domain-global")
+			.desc("Enable global domain").hasArg().type(Boolean.class).required(false).build();
+
+	public static final Option OPTION_DOMAIN_DETAILED = Option.builder("dd").longOpt("domain-detailed")
+			.desc("Enable detailed subdomains").hasArg().type(Boolean.class).required(false).build();
+
+	public static final Option OPTION_GRANULARITY = Option.builder("g").longOpt("granularity")
+			.desc("Granularity:\nGLOBAL, UNIVERSITY, DEPARTMENT").hasArg().type(String.class).required(false).build();
+
+	public static final Option OPTION_SEED = Option.builder("s").longOpt("seed").desc("Seed").hasArg()
+			.type(Number.class).required(false).build();
+
+	public static final Option OPTION_UNIVERSITY_OFFSET = Option.builder("uo").longOpt("university-offset")
+			.desc("University offset").hasArg().type(Number.class).required(false).build();
+
+	public static final Option OPTION_UNIVERSITY_AMOUNT = Option.builder("ua").longOpt("university-amount")
+			.desc("University amount").hasArg().type(Number.class).required(false).build();
+
+	public static final Option OPTION_UNIVERSITY_LIMIT = Option.builder("ul").longOpt("university-limit")
+			.desc("University limit").hasArg().type(Number.class).required(false).build();
+
+	public static final Option OPTION_DEPARTMENT_LIMIT = Option.builder("dl").longOpt("department-limit")
+			.desc("Department limit").hasArg().type(Number.class).required(false).build();
+
+	public static final Option OPTION_UNIVERSITY_TEMPLATE = Option.builder("ut").longOpt("university-template")
+			.desc("URI template for universities").hasArg().type(String.class).required(false).build();
+
+	public static final Option OPTION_DEPARTMENT_TEMPLATE = Option.builder("dt").longOpt("department-template")
+			.desc("URI template for departments").hasArg().type(String.class).required(false).build();
 
 	public static final Options getOptions() {
 		Options options = new Options();
-		// options.addOption(OPTION_PORT);
-		// options.addOption(OPTION_STORAGE_DIRECTORY);
-		// options.addOption(OPTION_TEMPORARY_STORAGE_DIRECTORY);
+		options.addOption(OPTION_IMAGE_ONTOLOGY);
+		options.addOption(OPTION_IMAGE_GLOBAL);
+		options.addOption(OPTION_IMAGE_UNIVERSITY);
+		options.addOption(OPTION_IMAGE_DEPARTMENT);
+		options.addOption(OPTION_NETWORK);
+		options.addOption(OPTION_DOMAIN);
+		options.addOption(OPTION_DOMAIN_GLOBAL);
+		options.addOption(OPTION_DOMAIN_DETAILED);
+		options.addOption(OPTION_GRANULARITY);
+		options.addOption(OPTION_SEED);
+		options.addOption(OPTION_UNIVERSITY_OFFSET);
+		options.addOption(OPTION_UNIVERSITY_AMOUNT);
+		options.addOption(OPTION_UNIVERSITY_LIMIT);
+		options.addOption(OPTION_DEPARTMENT_LIMIT);
+		options.addOption(OPTION_UNIVERSITY_TEMPLATE);
+		options.addOption(OPTION_DEPARTMENT_TEMPLATE);
 		return options;
 	}
 
@@ -49,80 +100,193 @@ public class App {
 		HelpFormatter helpFormatter = new HelpFormatter();
 		helpFormatter.setLeftPadding(4);
 
-		// Try to parse the options and start the controller
 		try {
 
-			// Parse the command line options
 			commandLine = parser.parse(getOptions(), args);
 
 			Configuration configuration = new Configuration();
 
-			configuration.setProxyNetwork("traefik_proxy");
-			configuration.getImages().put(Level.ONTOLOGY, "fekepp/dlubm:1.9.0-pr");
-			configuration.getImages().put(Level.GLOBAL, "fekepp/dlubm:1.9.0-pr");
-			configuration.getImages().put(Level.UNIVERSITY, "fekepp/dlubm:1.9.0-pr");
-			// configuration.getImages().put(Level.UNIVERSITY,
-			// "fekepp/dlubm-scal:latest");
-			configuration.getImages().put(Level.DEPARTMENT, "fekepp/dlubm:1.9.0-pr");
-			configuration.setDomain("dlubm.ddns.me");
-			configuration.setWithDetailedSubdomain(false);
-			configuration.setWithGlobalDomain(true);
+			Option option = null;
+			Object value = null;
 
-			configuration.setOntology(null);
-			configuration.setSeed(0);
-			configuration.setGranularity(Level.DEPARTMENT);
-			configuration.setUniversityOffset(0);
-			configuration.setUniversityAmount(5);
-			configuration.setUniversityLimit(1);
-			configuration.setDepartmentLimit(1);
-			configuration.setUniversityTemplate(null);
-			configuration.setDepartmentTemplate(null);
+			option = OPTION_IMAGE_ONTOLOGY;
+			value = commandLine.getParsedOptionValue(option.getOpt());
+			if (value != null) {
+				if (value instanceof String && ((String) value).length() > 0) {
+					configuration.getImages().put(Level.ONTOLOGY, (String) value);
+				} else {
+					throw new ParseException("Bad input for option \"-" + option.getOpt() + ", --" + option.getLongOpt()
+							+ "\": " + value);
+				}
+			}
+
+			option = OPTION_IMAGE_GLOBAL;
+			value = commandLine.getParsedOptionValue(option.getOpt());
+			if (value != null) {
+				if (value instanceof String && ((String) value).length() > 0) {
+					configuration.getImages().put(Level.GLOBAL, (String) value);
+				} else {
+					throw new ParseException("Bad input for option \"-" + option.getOpt() + ", --" + option.getLongOpt()
+							+ "\": " + value);
+				}
+			}
+
+			option = OPTION_IMAGE_UNIVERSITY;
+			value = commandLine.getParsedOptionValue(option.getOpt());
+			if (value != null) {
+				if (value instanceof String && ((String) value).length() > 0) {
+					configuration.getImages().put(Level.UNIVERSITY, (String) value);
+				} else {
+					throw new ParseException("Bad input for option \"-" + option.getOpt() + ", --" + option.getLongOpt()
+							+ "\": " + value);
+				}
+			}
+
+			option = OPTION_IMAGE_DEPARTMENT;
+			value = commandLine.getParsedOptionValue(option.getOpt());
+			if (value != null) {
+				if (value instanceof String && ((String) value).length() > 0) {
+					configuration.getImages().put(Level.DEPARTMENT, (String) value);
+				} else {
+					throw new ParseException("Bad input for option \"-" + option.getOpt() + ", --" + option.getLongOpt()
+							+ "\": " + value);
+				}
+			}
+
+			option = OPTION_NETWORK;
+			value = commandLine.getParsedOptionValue(option.getOpt());
+			if (value != null) {
+				if (value instanceof String && ((String) value).length() > 0) {
+					configuration.setProxyNetwork((String) value);
+				} else {
+					throw new ParseException("Bad input for option \"-" + option.getOpt() + ", --" + option.getLongOpt()
+							+ "\": " + value);
+				}
+			}
+
+			option = OPTION_DOMAIN;
+			value = commandLine.getParsedOptionValue(option.getOpt());
+			if (value != null) {
+				if (value instanceof String && ((String) value).length() > 0) {
+					configuration.setDomain((String) value);
+				} else {
+					throw new ParseException("Bad input for option \"-" + option.getOpt() + ", --" + option.getLongOpt()
+							+ "\": " + value);
+				}
+			}
+
+			option = OPTION_DOMAIN_GLOBAL;
+			value = commandLine.getParsedOptionValue(option.getOpt());
+			if (value != null) {
+				if (value instanceof Boolean) {
+					configuration.setWithGlobalDomain((Boolean) value);
+				} else {
+					throw new ParseException("Bad input for option \"-" + option.getOpt() + ", --" + option.getLongOpt()
+							+ "\": " + value);
+				}
+			}
+
+			option = OPTION_DOMAIN_DETAILED;
+			value = commandLine.getParsedOptionValue(option.getOpt());
+			if (value != null) {
+				if (value instanceof Boolean) {
+					configuration.setWithDetailedSubdomain((Boolean) value);
+				} else {
+					throw new ParseException("Bad input for option \"-" + option.getOpt() + ", --" + option.getLongOpt()
+							+ "\": " + value);
+				}
+			}
+
+			option = OPTION_GRANULARITY;
+			value = commandLine.getParsedOptionValue(option.getOpt());
+			if (value != null) {
+				if (value instanceof String && ((String) value).length() > 0 && Level.valueOf((String) value) != null) {
+					configuration.setGranularity(Level.valueOf((String) value));
+				} else {
+					throw new ParseException("Bad input for option \"-" + option.getOpt() + ", --" + option.getLongOpt()
+							+ "\": " + value);
+				}
+			}
+
+			option = OPTION_SEED;
+			value = commandLine.getParsedOptionValue(option.getOpt());
+			if (value != null) {
+				if (value instanceof Long && (Long) value >= 0 && (Long) value <= 65535) {
+					configuration.setSeed(((Long) value).intValue());
+				} else {
+					throw new ParseException("Bad input for option \"-" + option.getOpt() + ", --" + option.getLongOpt()
+							+ "\": " + value);
+				}
+			}
+
+			option = OPTION_UNIVERSITY_OFFSET;
+			value = commandLine.getParsedOptionValue(option.getOpt());
+			if (value != null) {
+				if (value instanceof Long && (Long) value >= 0 && (Long) value <= 65535) {
+					configuration.setUniversityOffset(((Long) value).intValue());
+				} else {
+					throw new ParseException("Bad input for option \"-" + option.getOpt() + ", --" + option.getLongOpt()
+							+ "\": " + value);
+				}
+			}
+
+			option = OPTION_UNIVERSITY_AMOUNT;
+			value = commandLine.getParsedOptionValue(option.getOpt());
+			if (value != null) {
+				if (value instanceof Long && (Long) value > 0 && (Long) value <= 65535) {
+					configuration.setUniversityOffset(((Long) value).intValue());
+				} else {
+					throw new ParseException("Bad input for option \"-" + option.getOpt() + ", --" + option.getLongOpt()
+							+ "\": " + value);
+				}
+			}
+
+			option = OPTION_UNIVERSITY_LIMIT;
+			value = commandLine.getParsedOptionValue(option.getOpt());
+			if (value != null) {
+				if (value instanceof Long && (Long) value > 0 && (Long) value <= 65535) {
+					configuration.setUniversityOffset(((Long) value).intValue());
+				} else {
+					throw new ParseException("Bad input for option \"-" + option.getOpt() + ", --" + option.getLongOpt()
+							+ "\": " + value);
+				}
+			}
+
+			option = OPTION_DEPARTMENT_LIMIT;
+			value = commandLine.getParsedOptionValue(option.getOpt());
+			if (value != null) {
+				if (value instanceof Long && (Long) value > 0 && (Long) value <= 65535) {
+					configuration.setUniversityOffset(((Long) value).intValue());
+				} else {
+					throw new ParseException("Bad input for option \"-" + option.getOpt() + ", --" + option.getLongOpt()
+							+ "\": " + value);
+				}
+			}
+
+			option = OPTION_UNIVERSITY_TEMPLATE;
+			value = commandLine.getParsedOptionValue(option.getOpt());
+			if (value != null) {
+				if (value instanceof String && ((String) value).length() > 0) {
+					configuration.setUniversityTemplate((String) value);
+				} else {
+					throw new ParseException("Bad input for option \"-" + option.getOpt() + ", --" + option.getLongOpt()
+							+ "\": " + value);
+				}
+			}
+
+			option = OPTION_DEPARTMENT_TEMPLATE;
+			value = commandLine.getParsedOptionValue(option.getOpt());
+			if (value != null) {
+				if (value instanceof String && ((String) value).length() > 0) {
+					configuration.setDepartmentTemplate((String) value);
+				} else {
+					throw new ParseException("Bad input for option \"-" + option.getOpt() + ", --" + option.getLongOpt()
+							+ "\": " + value);
+				}
+			}
 
 			Composer composer = new Composer(configuration);
 			composer.compose();
-
-			// // Create a controller
-			// ServerController controller = new ServerController();
-			//
-			// // Configure port
-			// Object portOptionParsedValue =
-			// commandLine.getParsedOptionValue(OPTION_PORT.getOpt());
-			// if (portOptionParsedValue instanceof Long && (Long)
-			// portOptionParsedValue > 0
-			// && (Long) portOptionParsedValue <= 65535) {
-			// controller.setPort(((Long) portOptionParsedValue).intValue());
-			// } else {
-			// throw new ParseException("Bad input string: " +
-			// portOptionParsedValue);
-			// }
-			//
-			// // Configure storage directory
-			// Object storageDirectoryOptionParsedValue = commandLine
-			// .getParsedOptionValue(OPTION_STORAGE_DIRECTORY.getOpt());
-			// if (storageDirectoryOptionParsedValue instanceof String
-			// && ((String) storageDirectoryOptionParsedValue).length() > 0) {
-			// controller.setStorageDirectory(Paths.get((String)
-			// storageDirectoryOptionParsedValue));
-			// } else {
-			// throw new ParseException("Bad input string: " +
-			// storageDirectoryOptionParsedValue);
-			// }
-			//
-			// // Configure temporary storage directory
-			// Object temporaryStorageDirectoryOptionParsedValue = commandLine
-			// .getParsedOptionValue(OPTION_TEMPORARY_STORAGE_DIRECTORY.getOpt());
-			// if (temporaryStorageDirectoryOptionParsedValue instanceof String
-			// && ((String) temporaryStorageDirectoryOptionParsedValue).length()
-			// > 0) {
-			// controller.setStorageDirectoryTemporary(Paths.get((String)
-			// temporaryStorageDirectoryOptionParsedValue));
-			// } else {
-			// throw new ParseException("Bad input string: " +
-			// temporaryStorageDirectoryOptionParsedValue);
-			// }
-			//
-			// // Start the controller
-			// controller.startBlocking();
 
 		}
 
